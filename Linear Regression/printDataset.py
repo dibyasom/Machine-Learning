58474f3eceb1ce78f32bfc3aa8f288bab23811a7
vhaegar1526 <dibya.secret@gmail.com>
2020-08-24 01:32:49 +0530

    Deploying a Linear Regression model from scratch, w/o libraries

---
+++ Linear Regression/printDataset.py
@@ -0,0 +1,8 @@
+import pandas as pd #Required for importing and manipulating csv files.
+
+dataset1 = pd.read_csv('headbrain.csv') #Given dataset.
+dataset2 = pd.read_csv('null.bin') #Same :)
+
+print(dataset1) #Printing the dataset, to analyse and check for
+
+print(dataset2) #dtype and other attributes of dataet.
